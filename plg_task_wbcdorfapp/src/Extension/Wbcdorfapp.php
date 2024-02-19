26e654fc2c046fa35742e4307e5d393f98918c10
Claudia Oerter <coweb01@users.noreply.github.com>
2024-02-19 14:16:56 +0100

    Datenübernahme aus DorfaPP

--- plg_task_wbcdorfapp/src/Extension/Wbcdorfapp.php
+++ plg_task_wbcdorfapp/src/Extension/Wbcdorfapp.php
@@ -19,6 +19,11 @@ use Joomla\Event\SubscriberInterface;
 use Joomla\Filesystem\File;
 use Joomla\Filesystem\Path;
 use Joomla\Http\HttpFactory;
+use Joomla\CMS\Categories\Categories;
+use Joomla\CMS\Categories\CategoryNode;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
@@ -67,6 +72,24 @@ final class Wbcdorfapp extends CMSPlugin implements SubscriberInterface
      * @since 4.1.0
      */
     protected $autoloadLanguage = true;
+
+    /**
+     * The http factory
+     *
+     * @var    DAlist
+     * @since  4.2.0
+     */
+    protected $DAlist = array();
+
+
+    /**
+     * The Category in Joomla for Dorfapp Articles
+     *
+     * @var    $AppCategory
+     * @since  4.2.0
+     */
+    protected $AppCategory;
+
 
     /**
      * The http factory
@@ -175,6 +198,7 @@ final class Wbcdorfapp extends CMSPlugin implements SubscriberInterface
 
         $this->url               = $params->source_url;
         $this->source_type       = $params->source_type;
+        $this->AppCategory       = $params->appcategory;
         $responseUrl             = $this->url . '/' . $this->source_type . '/ids';
         $this->timeout           = $params->timeout;
         $this->auth              = (string) $params->auth ?? 0;
@@ -204,7 +228,7 @@ final class Wbcdorfapp extends CMSPlugin implements SubscriberInterface
         
         try {
             if ($this->source_type == 'articles' && $responseCode == 200) {
-                $output = $this->AppNewsArticles($responseBody); 
+                $output = $this->FetchAppNewsArticles($responseBody);
                 File::write($responseFilename, $output);
                 $this->snapshot['output_file'] = $responseFilename;
                 $responseStatus                = 'OK';
@@ -217,22 +241,28 @@ final class Wbcdorfapp extends CMSPlugin implements SubscriberInterface
             $responseStatus = 'NOT_SAVED';
         }
 
-        $this->snapshot['output']      = <<< EOF
-======= Task Output Body =======
-> URL: $url
-> Response Code: $responseCode
-> Response: $responseStatus
-> 
-EOF;
-
         $this->logTask(sprintf($this->getApplication()->getLanguage()->_('PLG_WBCDORFAPP_REQUESTS_TASK_GET_REQUEST_LOG_RESPONSE'), $responseCode));
 
         if ($response->code !== 200) {
             return TaskStatus::KNOCKOUT;
         }
 
-        return TaskStatus::OK;
+        if ($this->WriteArticles()) {
+
+            $this->snapshot['output']      = <<< EOF
+            ======= Task Output Body =======
+            > URL: $url
+            > Response Code: $responseCode
+            > Response: $responseStatus
+            > 
+            EOF;
+
+            return TaskStatus::OK;
+        } else {
+            return TaskStatus::KNOCKOUT;
+        }
     }
+
     /**
      * 
      * Get Articles from the API
@@ -244,8 +274,7 @@ EOF;
      * @since 4.1.0
      * @throws \Exception
      */
-    protected function AppNewsArticles($responseBody) {
-        $list = array();
+    protected function FetchAppNewsArticles($responseBody) {
         $output = '';
         $itemIds = json_decode($responseBody);
 
@@ -261,25 +290,191 @@ EOF;
                 $this->logTask($this->getApplication()->getLanguage()->_('PLG_WBCDORFAPP_REQUESTS_TASK_GET_REQUEST_LOG_TIMEOUT'));
                 return TaskStatus::TIMEOUT;
             }
+            if ($responsearticle->code == 200) {
+                $item = json_decode($responsearticle->body);
 
-            $item = json_decode($responsearticle->body);
-            $list[ $itemId->id]['id'] = $itemId;
-            $list[ $itemId->id]['title'] = $item->text;
-            $list[ $itemId->id]['introtext'] = $item->summary;
-            $list[$itemId]['fulltext'] = $item->content;
-            $list[$itemId]['heading'] = $item->heading;
-            // Bild auslesen
-            $assetReference = $item->coverAssetReference;
-            $list[$itemId]['image']['imageSrc'] = $assetReference->assetImage->assetImageUrl;
-            $imageTitle = $assetReference->assetImage->text; 
-            $list[$itemId]['image']['imageTitle'] = $imageTitle;
-            $list[$itemId]['image']['imageQuelle'] = $assetReference->assetImage->source;
-           
-            $output .= $itemId . "\n\n";
-            $output .= $item->text . "\n\n";
-            $output .= '<------------------------------------------>' . "\n";
+                if (empty($item) || empty($item->text)) { // wenn kein Text vorhanden ist, dann weiter
+                    continue;
+                }
+
+                $this->DAlist[$itemId]['id'] = $item->id;
+                $this->DAlist[$itemId]['title'] = $item->text;
+                $this->DAlist[$itemId]['fulltext'] = '';
+
+                if (!empty($item->summary)) {
+                    $this->DAlist[$itemId]['introtext'] = $item->summary;
+                    $this->DAlist[$itemId]['fulltext']  = (!empty($item->content)) ? $item->content : '';
+                } else {
+                    $this->DAlist[$itemId]['introtext'] = (!empty($item->content)) ? $item->content : '';
+                }
+                $this->DAlist[$itemId]['spitzmarke'] = (!empty($item->heading)) ? $item->heading : '';
+    
+                // Bild auslesen
+                $this->DAlist[$itemId]['image'] = array();
+                $assetReference = $item->coverAssetReference;
+                if (!empty($assetReference->assetImage->assetImageUrl)){
+                    $this->DAlist[$itemId]['image']['imageSrc']    = $assetReference->assetImage->assetImageUrl;
+                    $imageTitle = $assetReference->assetImage->text; 
+                    $this->DAlist[$itemId]['image']['imageTitle']  = (!empty($assetReference->assetImage->texte)) ? $assetReference->assetImage->texte : '';
+                    $this->DAlist[$itemId]['image']['imageQuelle'] = (!empty($assetReference->assetImage->source)) ? $assetReference->assetImage->source : '';
+                }
+                $output .= $itemId . "\n\n";
+                $output .= $item->text . "\n\n";
+                $output .= '<------------------------------------------>' . "\n";
+            }
+            
         }
-
         return $output;
     }
+
+    /**
+    * 
+    * Create Category in Joomla
+    * 
+    *
+    * @return 
+    *
+    * @since 4.1.0
+    * @throws \Exception
+    */
+    protected function DeleteArticles() {
+        $app          = \Joomla\CMS\Factory::getApplication();
+        $mvcFactory   = $app->bootComponent('com_content')->getMVCFactory();
+        $articleIds   = array();
+        $categories   = Categories::getInstance('content');
+        $category     = $categories->get($this->AppCategory);
+        if (!$category) {
+            throw new Exception(Text::_('PLG_TASK_WBCDORFAPP_KATEGORIE_NOT_EXIST') );
+            return false;
+        }
+        
+        // Create the Model to get Article from Category
+        $articlesModel = $mvcFactory->createModel('Articles', 'Administrator', ['ignore_request' => true]);
+        
+        /* Set filter for category id */
+        $articlesModel->setState('filter.category_id', $category->id);
+        $articlesModel->setState('filter.published', '*');
+        $articles = $articlesModel->getItems();
+        if (empty($articles)) {
+            return true;
+        }                
+        
+        foreach ($articles as $article) {
+            // create the model to delete the article
+            $articleModel = $mvcFactory->createModel('Article', 'Administrator', ['ignore_request' => true]);
+            $articleIds[] = $article->id;
+            $article      = [
+                'id'            => $article->id,
+                'state'         => -2 // -2 = Trashed 
+            ];
+            if (!$articleModel->save($article)){
+                throw new Exception(Text::_('PLG_TASK_WBCDORFAPP_ARTICLE_ERROR_SAVE_STATE') . $article->id); 
+                continue;
+            }
+            
+        }
+       
+        // delete article
+        if ( !$articleModel->delete($articleIds)) {
+            throw new Exception(Text::_('PLG_TASK_WBCDORFAPP_ARTICLE_NOT_DELETE')); 
+            return false;
+        }
+        return true;
+    }
+    /**
+     * 
+     * Write Articles in Joomla
+     * 
+     *
+     * @return 
+     *
+     * @since 4.1.0
+     * @throws \Exception
+     */
+    protected function WriteArticles() {
+        if ($this->ExistCategory($this->AppCategory) === false) {
+            //$catId = CreateCategory();
+            throw new Exception( Text::_('PLG_TASK_WBCDORFAPP_KATEGORIE_NOT_EXIST'));
+            return false;
+        } 
+        if (empty($this->DAlist)) {
+            throw new Exception(Text::_('PLG_TASK_WBCDORFAPP_NO_ARTICLES_FOUND_IN_SOURCE'));
+            return false;
+        }
+        // Löschen aller vorhandenen Artikel in dieser Kategorie!
+        $this->DeleteArticles();
+
+        $app          = \Joomla\CMS\Factory::getApplication();
+        $mvcFactory   = $app->bootComponent('com_content')->getMVCFactory();
+
+        foreach ($this->DAlist as $item){   
+            // create the model to save the article
+            $articleModel = $mvcFactory->createModel('Article', 'Administrator', ['ignore_request' => true]);
+
+            // Joomla Images 
+            $images = array();
+            if (!empty($item['image'])) {
+                $images = [
+                    'image_intro'               => $item['image']['imageSrc'],
+                    'float_intro'               => '',
+                    'image_intro_alt'           => $item['image']['imageTitle'],
+                    'image_intro_caption'       => '',
+                    'image_fulltext'            => $item['image']['imageSrc'],
+                    'float_fulltext'            => '',
+                    'image_fulltext_alt'        => $item['image']['imageTitle'],
+                    'image_fulltext_caption'    => '',
+                ];
+                $images = json_encode($images);
+            }
+
+            $article = [
+                'catid'         => $this->AppCategory ,
+                'alias'         => \Joomla\CMS\Filter\OutputFilter::stringURLSafe($item['title']),
+                'title'         => $item['title'],
+                'introtext'     => $item['introtext'],
+                'fulltext'      => $item['fulltext'],
+                'state'         => 1,
+                'images'        => !empty($images) ? $images : '',
+                'language'      => '*',
+                ];
+
+            if (!$articleModel->save($article))
+            {
+                throw new Exception(Text::_('PLG_TASK_WBCDORFAPP_ARTICLE_ERROR_SAVE')); 
+                return false;          
+            }
+        }
+        return true;
+    }
+     /**
+     * 
+     * Check if Category exists
+     * 
+     *
+     * @return 
+     *
+     * @since 4.1.0
+     * @throws \Exception
+     */
+    protected function ExistCategory($id) {
+
+        $categories = Categories::getInstance('content');
+        $category   = $categories->get($id);
+        if ($category) {
+            return true;
+        }
+        return false;
+    }
+    /**
+     * 
+     * Create Category in Joomla
+     * 
+     *
+     * @return 
+     *
+     * @since 4.1.0
+     * @throws \Exception
+     */
+    protected function CreateCategory() {}
+
 }
